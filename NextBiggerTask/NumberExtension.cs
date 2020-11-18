e96604605325b13762fb2e395c5556413975cb7d
dmitri_shepelev <shepelev.dm@gmail.com>
2020-11-18 20:33:57 +0300

    18.11 Done

--- NextBiggerTask/NumberExtension.cs
+++ NextBiggerTask/NumberExtension.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 
 namespace NextBiggerTask
 {
@@ -14,7 +15,47 @@ namespace NextBiggerTask
         /// <exception cref="ArgumentException">Thrown when source number is less than 0.</exception>
         public static int? NextBiggerThan(int number)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (number < 0)
+            {
+                throw new ArgumentException($"Value of {nameof(number)} cannot be less zero.");
+            }
+
+            if (number == int.MaxValue)
+            {
+                return null;
+            }
+
+            var tail = new List<int>();
+
+            while (number != 0)
+            {
+                var lastNumeral = number % 10;
+                var prevNumeral = (number / 10) % 10;
+                if (lastNumeral > prevNumeral && prevNumeral != 0)
+                {
+                    number /= 100;
+                    number = (number * 10) + lastNumeral;
+                    tail.Add(prevNumeral);
+                    break;
+                }
+
+                tail.Add(lastNumeral);
+                number /= 10;
+            }
+
+            if (number == 0)
+            {
+                return null;
+            }
+
+            tail.Sort();
+            var result = number;
+            for (int i = 0; i < tail.Count; i++)
+            {
+                result = (result * 10) + tail[i];
+            }
+
+            return result;
         }
     }
 }
